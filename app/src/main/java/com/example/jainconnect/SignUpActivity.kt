3b3eacc88fa280679af853f91289eea98f323de3
Suhani Ranka <suhanijainn006@gmail.com>
2025-09-12 14:06:04 +0530

    will add firebase

--- app/src/main/java/com/example/jainconnect/SignUpActivity.kt
+++ app/src/main/java/com/example/jainconnect/SignUpActivity.kt
@@ -8,11 +8,11 @@ import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
+
 
 class SignUpActivity : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
+
     private lateinit var etEmail: EditText
     private lateinit var etPassword: EditText
     private lateinit var etConfirmPassword: EditText
@@ -23,7 +23,7 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        auth = FirebaseAuth.getInstance()
+
 
         etEmail = findViewById(R.id.etEmail)
         etPassword = findViewById(R.id.etPassword)
@@ -51,17 +51,7 @@ class SignUpActivity : AppCompatActivity() {
 
             progressBar.visibility = android.view.View.VISIBLE
 
-            auth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener { task ->
-                    progressBar.visibility = android.view.View.GONE
-                    if (task.isSuccessful) {
-                        Toast.makeText(this, "Signup successful", Toast.LENGTH_SHORT).show()
-                        startActivity(Intent(this, LoginActivity::class.java))
-                        finish()
-                    } else {
-                        Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
-                    }
-                }
+
         }
     }
 }
